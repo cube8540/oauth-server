92d71507b0f821d4e9119b2bdc46e33d92bfdf94
cube8540 <cube8540@gmail.com>
2019-12-24 22:28:46 +0900

    프로젝트 커밋

---
+++ src/test/java/cube8540/oauth/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package cube8540.oauth.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
