497f3261ab41f93fa41ec6cce3b9efbca85f6655
cube8540 <cube8540@gmail.com>
2019-12-24 22:57:19 +0900

    데이터베이스 연결 설정 추가 및 테스트 코드 엑티브 프로파일 설정

--- src/test/java/cube8540/oauth/authentication/AuthenticationApplicationTests.java
+++ src/test/java/cube8540/oauth/authentication/AuthenticationApplicationTests.java
@@ -1,12 +1,17 @@
 package cube8540.oauth.authentication;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
+@DisplayName("스프링 컨테이너 테스트")
 class AuthenticationApplicationTests {
 
     @Test
+    @DisplayName("스프링 컨테이너 로딩")
     void contextLoads() {
     }
 
