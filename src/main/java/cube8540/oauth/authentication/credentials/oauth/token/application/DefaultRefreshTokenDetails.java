e3e14c0b219a9a62308d32b0186bd077653f5b09
cube8540 <cube8540@gmail.com>
2020-01-24 22:55:11 +0900

    기본 토큰 부여 서비스 구현

---
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/token/application/DefaultRefreshTokenDetails.java
@@ -0,0 +1,41 @@
+package cube8540.oauth.authentication.credentials.oauth.token.application;
+
+import cube8540.oauth.authentication.credentials.oauth.token.OAuth2RefreshTokenDetails;
+import cube8540.oauth.authentication.credentials.oauth.token.domain.OAuth2AuthorizedRefreshToken;
+
+import java.time.LocalDateTime;
+
+public class DefaultRefreshTokenDetails implements OAuth2RefreshTokenDetails {
+
+    private final String tokenValue;
+    private final LocalDateTime expiration;
+    private final boolean isExpired;
+    private final long expires;
+
+    public DefaultRefreshTokenDetails(OAuth2AuthorizedRefreshToken refreshToken) {
+        this.tokenValue = refreshToken.getTokenId().getValue();
+        this.expiration = refreshToken.getExpiration();
+        this.isExpired = refreshToken.isExpired();
+        this.expires = refreshToken.expiresIn();
+    }
+
+    @Override
+    public String tokenValue() {
+        return tokenValue;
+    }
+
+    @Override
+    public LocalDateTime expiration() {
+        return expiration;
+    }
+
+    @Override
+    public boolean isExpired() {
+        return isExpired;
+    }
+
+    @Override
+    public int expiresIn() {
+        return Long.valueOf(expires).intValue();
+    }
+}
--- src/main/java/cube8540/oauth/authentication/credentials/oauth/token/domain/OAuth2AccessTokenRepository.java
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/token/domain/OAuth2AccessTokenRepository.java
@@ -1,6 +1,13 @@
 package cube8540.oauth.authentication.credentials.oauth.token.domain;
 
+import cube8540.oauth.authentication.credentials.oauth.client.domain.OAuth2ClientId;
+import cube8540.oauth.authentication.users.domain.UserEmail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OAuth2AccessTokenRepository extends JpaRepository<OAuth2AuthorizedAccessToken, OAuth2TokenId> {
+
+    Optional<OAuth2AuthorizedAccessToken> findByClientAndEmail(OAuth2ClientId clientId, UserEmail email);
+
 }
