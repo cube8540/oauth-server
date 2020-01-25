65c0b0691325c8fe4a40061e300c45862da00936
cube8540 <cube8540@gmail.com>
2020-01-25 16:48:38 +0900

    OAuth2AuthorizationCodeService 구현

---
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/code/application/DefaultAuthorizationCodeService.java
@@ -0,0 +1,55 @@
+package cube8540.oauth.authentication.credentials.oauth.code.application;
+
+import cube8540.oauth.authentication.credentials.oauth.AuthorizationRequest;
+import cube8540.oauth.authentication.credentials.oauth.code.domain.AuthorizationCode;
+import cube8540.oauth.authentication.credentials.oauth.code.domain.AuthorizationCodeGenerator;
+import cube8540.oauth.authentication.credentials.oauth.code.domain.AuthorizationCodeRepository;
+import cube8540.oauth.authentication.credentials.oauth.code.domain.OAuth2AuthorizationCode;
+import cube8540.oauth.authentication.credentials.oauth.code.infra.DefaultAuthorizationCodeGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class DefaultAuthorizationCodeService implements OAuth2AuthorizationCodeService {
+
+    private final AuthorizationCodeRepository codeRepository;
+
+    private AuthorizationCodeGenerator codeGenerator = new DefaultAuthorizationCodeGenerator();
+    private Duration codeDuration = Duration.ofMinutes(1);
+
+    @Autowired
+    public DefaultAuthorizationCodeService(AuthorizationCodeRepository codeRepository) {
+        this.codeRepository = codeRepository;
+    }
+
+    @Override
+    public Optional<OAuth2AuthorizationCode> consume(AuthorizationCode code) {
+        Optional<OAuth2AuthorizationCode> authorizationCode = codeRepository.findById(code);
+        authorizationCode.ifPresent(codeRepository::delete);
+
+        return authorizationCode;
+    }
+
+    @Override
+    public AuthorizationCode generateNewAuthorizationCode(AuthorizationRequest request) {
+        LocalDateTime expiration = LocalDateTime.now().plus(codeDuration).withNano(0);
+        OAuth2AuthorizationCode authorizationCode = new OAuth2AuthorizationCode(codeGenerator, expiration);
+
+        authorizationCode.setAuthorizationRequest(request);
+        codeRepository.save(authorizationCode);
+
+        return authorizationCode.getCode();
+    }
+
+    public void setCodeGenerator(AuthorizationCodeGenerator codeGenerator) {
+        this.codeGenerator = codeGenerator;
+    }
+
+    public void setCodeDuration(Duration codeDuration) {
+        this.codeDuration = codeDuration;
+    }
+}
