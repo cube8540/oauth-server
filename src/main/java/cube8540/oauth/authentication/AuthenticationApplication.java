4fd2c72d2d0afe20be9d77b64b45de05669b9c28
cube8540 <cube8540@gmail.com>
2020-01-29 22:13:16 +0900

    기본 타임존 설정

--- src/main/java/cube8540/oauth/authentication/AuthenticationApplication.java
+++ src/main/java/cube8540/oauth/authentication/AuthenticationApplication.java
@@ -3,8 +3,18 @@ package cube8540.oauth.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AuthenticationApplication {
+
+    public static final TimeZone DEFAULT_TIME_ZONE = TimeZone.getTimeZone("Asia/Seoul");
+
+    @PostConstruct
+    void systemSetup() {
+        TimeZone.setDefault(DEFAULT_TIME_ZONE);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
