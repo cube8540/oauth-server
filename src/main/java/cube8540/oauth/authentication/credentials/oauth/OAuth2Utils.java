01ac4f7245e512094104cdffcd0815c4a7a8930e
cube8540 <cube8540@gmail.com>
2020-02-01 01:55:24 +0900

    OAuth2AccessTokenDetails 직렬화 클래스 추가

--- src/main/java/cube8540/oauth/authentication/credentials/oauth/OAuth2Utils.java
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/OAuth2Utils.java
@@ -29,6 +29,19 @@ public final class OAuth2Utils {
         public static final String SCOPE = "scope";
     }
 
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static final class AccessTokenSerializeKey {
+        public static final String ACCESS_TOKEN = "access_token";
+
+        public static final String TOKEN_TYPE = "token_type";
+
+        public static final String EXPIRES_IN = "expires_in";
+
+        public static final String REFRESH_TOKEN = "refresh_token";
+
+        public static final String SCOPE = "scope";
+    }
+
     public static Set<String> extractScopes(String value) {
         Set<String> result = new HashSet<>();
         if (value != null && value.trim().length() > 0) {
