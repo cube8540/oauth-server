b6ff613b038e017f9c88e955134147069d69e9aa
cube8540 <cube8540@gmail.com>
2020-02-01 01:55:01 +0900

    NPE 에러 수정

--- src/main/java/cube8540/oauth/authentication/credentials/oauth/token/application/DefaultAccessTokenDetails.java
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/token/application/DefaultAccessTokenDetails.java
@@ -30,18 +30,15 @@ public class DefaultAccessTokenDetails implements OAuth2AccessTokenDetails {
         this.tokenValue = accessToken.getTokenId().getValue();
         this.clientId = accessToken.getClient();
         this.scopeId = Collections.unmodifiableSet(accessToken.getScope());
-        this.username = accessToken.getEmail().getValue();
         this.expiration = accessToken.getExpiration();
         this.expiresIn = accessToken.expiresIn();
         this.isExpired = accessToken.isExpired();
+        this.tokenType = TOKEN_TYPE;
+        this.username = accessToken.getEmail() != null ? accessToken.getEmail().getValue() : null;
         this.refreshTokenDetails = accessToken.getRefreshToken() != null ?
                 new DefaultRefreshTokenDetails(accessToken.getRefreshToken()) : null;
-        this.tokenType = TOKEN_TYPE;
-        if (accessToken.getAdditionalInformation() != null) {
-            this.additionalInformation = Collections.unmodifiableMap(accessToken.getAdditionalInformation());
-        } else {
-            this.additionalInformation = null;
-        }
+        this.additionalInformation = accessToken.getAdditionalInformation() != null ?
+                Collections.unmodifiableMap(accessToken.getAdditionalInformation()) : null;
     }
 
     @Override
