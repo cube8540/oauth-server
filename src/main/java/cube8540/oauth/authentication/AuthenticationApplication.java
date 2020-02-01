16caa0666fda8364f9bfe0fdd4fd71c630db5eca
cube8540 <cube8540@gmail.com>
2020-02-01 19:13:27 +0900

    OAuth2 Spring Security 설정 파일 분리 및 공통 PasswordEncoder Bean 생성 위치 변경

--- src/main/java/cube8540/oauth/authentication/AuthenticationApplication.java
+++ src/main/java/cube8540/oauth/authentication/AuthenticationApplication.java
@@ -2,6 +2,10 @@ package cube8540.oauth.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import java.time.Clock;
@@ -20,6 +24,12 @@ public class AuthenticationApplication {
     @PostConstruct
     void systemSetup() {
         TimeZone.setDefault(DEFAULT_TIME_ZONE);
+    }
+
+    @Bean
+    @Primary
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     public static void main(String[] args) {
