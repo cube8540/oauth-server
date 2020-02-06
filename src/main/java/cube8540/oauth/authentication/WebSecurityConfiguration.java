92cd74a4c2f752015ee758e4a3922bfc66dfb099
cube8540 <cube8540@gmail.com>
2020-02-06 21:40:38 +0900

    Spring Security 설정 변경

--- src/main/java/cube8540/oauth/authentication/WebSecurityConfiguration.java
+++ src/main/java/cube8540/oauth/authentication/WebSecurityConfiguration.java
@@ -1,11 +1,9 @@
-package cube8540.oauth.authentication.credentials.oauth;
+package cube8540.oauth.authentication;
 
-import cube8540.oauth.authentication.credentials.oauth.authorize.endpoint.AuthorizationEndpoint;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,11 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Order(2)
 @EnableWebSecurity
-public class AuthorizationEndpointSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String DEFAULT_LOGIN_PAGE = "/login";
     private static final String DEFAULT_LOGIN_PROCESS_URL = "/login";
-    private static final String DEFAULT_APPROVAL_PAGE = AuthorizationEndpoint.DEFAULT_APPROVAL_PAGE;
 
     @Setter(onMethod_ = {@Autowired, @Qualifier("defaultUserService")})
     private UserDetailsService userDetailsService;
@@ -33,9 +30,6 @@ public class AuthorizationEndpointSecurityConfiguration extends WebSecurityConfi
     @Setter
     private String loginProcessUrl = DEFAULT_LOGIN_PROCESS_URL;
 
-    @Setter
-    private String approvalUrl = DEFAULT_APPROVAL_PAGE;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
@@ -43,15 +37,10 @@ public class AuthorizationEndpointSecurityConfiguration extends WebSecurityConfi
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers()
-                .antMatchers("/oauth/authorize", approvalUrl)
-                .antMatchers(HttpMethod.GET, loginPage)
-                .antMatchers(HttpMethod.POST, loginProcessUrl)
-                .and()
-            .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .permitAll();
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .permitAll();
     }
 }
