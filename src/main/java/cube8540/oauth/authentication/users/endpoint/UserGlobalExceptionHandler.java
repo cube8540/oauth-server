e500d96c7d5f709fa7e6d70dde24d57d492d8d41
cube8540 <cube8540@gmail.com>
2020-02-12 00:46:29 +0900

    유저 글로벌 예외 처리 헨들러 추가

--- src/main/java/cube8540/oauth/authentication/users/endpoint/UserGlobalExceptionHandler.java
+++ src/main/java/cube8540/oauth/authentication/users/endpoint/UserGlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package cube8540.oauth.authentication.users.endpoint;
 
 import cube8540.oauth.authentication.message.ErrorResponseMessage;
 import cube8540.oauth.authentication.message.ResponseMessage;
+import cube8540.oauth.authentication.users.domain.UserAlreadyCertificationException;
 import cube8540.oauth.authentication.users.domain.UserAlreadyExistsException;
+import cube8540.oauth.authentication.users.domain.UserExpiredException;
 import cube8540.oauth.authentication.users.domain.UserInvalidException;
 import cube8540.oauth.authentication.users.domain.UserNotFoundException;
+import cube8540.oauth.authentication.users.domain.UserNotMatchedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,6 +42,33 @@ public class UserGlobalExceptionHandler {
             log.warn("User not found handle {}, {}", e.getClass(), e.getMessage());
         }
         ResponseMessage message = ErrorResponseMessage.notfound(e.getMessage());
+        return new ResponseEntity<>(message, message.getStatus());
+    }
+
+    @ExceptionHandler(UserExpiredException.class)
+    public ResponseEntity<ResponseMessage> exceptionHandle(UserExpiredException e) {
+        if (log.isWarnEnabled()) {
+            log.warn("User expired handle {}, {}", e.getClass(), e.getMessage());
+        }
+        ResponseMessage message = ErrorResponseMessage.gone(e.getMessage());
+        return new ResponseEntity<>(message, message.getStatus());
+    }
+
+    @ExceptionHandler(UserNotMatchedException.class)
+    public ResponseEntity<ResponseMessage> exceptionHandle(UserNotMatchedException e) {
+        if (log.isWarnEnabled()) {
+            log.warn("User not matched handle {}, {}", e.getClass(), e.getMessage());
+        }
+        ResponseMessage message = ErrorResponseMessage.conflict(e.getMessage());
+        return new ResponseEntity<>(message, message.getStatus());
+    }
+
+    @ExceptionHandler(UserAlreadyCertificationException.class)
+    public ResponseEntity<ResponseMessage> exceptionHandle(UserAlreadyCertificationException e) {
+        if (log.isWarnEnabled()) {
+            log.warn("User already certification handle {}, {}", e.getClass(), e.getMessage());
+        }
+        ResponseMessage message = ErrorResponseMessage.conflict(e.getMessage());
         return new ResponseEntity<>(message, message.getStatus());
     }
 
