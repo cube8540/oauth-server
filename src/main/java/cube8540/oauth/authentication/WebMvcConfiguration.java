ddfa2f0992aa3d33b53ccf5d020a895a51492d8a
cube8540 <cube8540@gmail.com>
2020-02-13 22:24:43 +0900

    화면 엔드포인트 클래스 변경

---
+++ src/main/java/cube8540/oauth/authentication/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package cube8540.oauth.authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/accounts/signin").setViewName("accounts/signin");
+    }
+}
