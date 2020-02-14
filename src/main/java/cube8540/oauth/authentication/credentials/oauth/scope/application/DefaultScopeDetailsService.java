e04d18c338440e4c3768356a612bf164294bc4c2
cube8540 <cube8540@gmail.com>
2020-02-14 23:26:40 +0900

    인자로 받은 인증 정보로 사용자가 접근 가능한 스코프만 반환하는 API 추가

--- src/main/java/cube8540/oauth/authentication/credentials/oauth/scope/application/DefaultScopeDetailsService.java
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/scope/application/DefaultScopeDetailsService.java
@@ -1,10 +1,12 @@
 package cube8540.oauth.authentication.credentials.oauth.scope.application;
 
+import cube8540.oauth.authentication.credentials.oauth.scope.OAuth2AccessibleScopeDetailsService;
 import cube8540.oauth.authentication.credentials.oauth.scope.OAuth2ScopeDetails;
 import cube8540.oauth.authentication.credentials.oauth.scope.OAuth2ScopeDetailsService;
 import cube8540.oauth.authentication.credentials.oauth.scope.domain.OAuth2ScopeId;
 import cube8540.oauth.authentication.credentials.oauth.scope.domain.OAuth2ScopeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -12,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class DefaultScopeDetailsService implements OAuth2ScopeDetailsService {
+public class DefaultScopeDetailsService implements OAuth2ScopeDetailsService, OAuth2AccessibleScopeDetailsService {
 
     private final OAuth2ScopeRepository repository;
 
@@ -26,6 +28,12 @@ public class DefaultScopeDetailsService implements OAuth2ScopeDetailsService {
         List<OAuth2ScopeId> scopeIn = scopeIds.stream()
                 .map(OAuth2ScopeId::new).collect(Collectors.toList());
         return repository.findByIdIn(scopeIn).stream()
-                .map(DetailsOAuth2ScopeDetails::new).collect(Collectors.toList());
+                .map(DefaultOAuth2ScopeDetails::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<OAuth2ScopeDetails> readAccessibleScopes(Authentication authentication) {
+        return repository.findAll().stream().filter(scope -> scope.isAccessible(authentication))
+                .map(DefaultOAuth2ScopeDetails::new).collect(Collectors.toList());
     }
 }
--- src/test/java/cube8540/oauth/authentication/credentials/oauth/scope/application/DefaultScopeDetailsServiceTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/scope/application/DefaultScopeDetailsServiceTest.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.Authentication;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -35,17 +36,6 @@ class DefaultScopeDetailsServiceTest {
     @Nested
     @DisplayName("스코프 상세 검색")
     class LookupScopes {
-
-        private Collection<OAuth2Scope> scopes;
-
-        @BeforeEach
-        void setup() {
-            this.scopes = new ArrayList<>();
-
-            this.scopes.add(mock(OAuth2Scope.class));
-            this.scopes.add(mock(OAuth2Scope.class));
-            this.scopes.add(mock(OAuth2Scope.class));
-        }
 
         @Nested
         @DisplayName("검색 결과가 없을시")
@@ -89,7 +79,7 @@ class DefaultScopeDetailsServiceTest {
             void shouldReturnsScopeDetails() {
                 Collection<OAuth2ScopeDetails> results = service.loadScopeDetailsByScopeIds(parameters);
 
-                Collection<OAuth2ScopeDetails> expected = SCOPES.stream().map(DetailsOAuth2ScopeDetails::new).collect(Collectors.toList());
+                Collection<OAuth2ScopeDetails> expected = SCOPES.stream().map(DefaultOAuth2ScopeDetails::new).collect(Collectors.toList());
                 assertEquals(expected, results);
             }
 
@@ -100,6 +90,45 @@ class DefaultScopeDetailsServiceTest {
                 when(scope.getDescription()).thenReturn(description);
                 return scope;
             }
+        }
+    }
+
+    @Nested
+    @DisplayName("접근 가능한 스코프 읽기")
+    class ReadAccessibleScope {
+
+        private Authentication authentication;
+        private List<OAuth2Scope> accessibleScopes;
+
+        @BeforeEach
+        void setup() {
+            this.authentication = mock(Authentication.class);
+            this.accessibleScopes = Arrays.asList(mocking("SCOPE-1", authentication, true),
+                    mocking("SCOPE-2", authentication, true),
+                    mocking("SCOPE-3", authentication, true));
+            List<OAuth2Scope> cannotAccessibleScopes = Arrays.asList(mocking("SCOPE-4", authentication, false),
+                    mocking("SCOPE-5", authentication, false),
+                    mocking("SCOPE-6", authentication, false));
+
+            when(repository.findAll()).thenReturn(Stream.concat(accessibleScopes.stream(), cannotAccessibleScopes.stream()).collect(Collectors.toList()));
+        }
+
+        @Test
+        @DisplayName("저장소에서 반환된 스코프중 접근 가능한 스코프만 반환해야 한다.")
+        void shouldReturnsCanAccessibleScopes() {
+            Collection<OAuth2ScopeDetails> scopes = service.readAccessibleScopes(authentication);
+
+            List<OAuth2ScopeDetails> expected = this.accessibleScopes.stream()
+                    .map(DefaultOAuth2ScopeDetails::new).collect(Collectors.toList());
+            assertEquals(expected, scopes);
+        }
+
+        private OAuth2Scope mocking(String scopeId, Authentication authentication, boolean accessible) {
+            OAuth2Scope scope = mock(OAuth2Scope.class);
+
+            when(scope.getId()).thenReturn(new OAuth2ScopeId(scopeId));
+            when(scope.isAccessible(authentication)).thenReturn(accessible);
+            return scope;
         }
     }
 }
