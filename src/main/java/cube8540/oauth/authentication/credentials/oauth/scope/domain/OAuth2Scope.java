34cc54f93c9fa208a9408eead0c3b889ec014023
cube8540 <cube8540@gmail.com>
2020-02-14 23:25:00 +0900

    스코프에 접근 가능한 권한 추가 및 삭제와 접근 가능 여부 확인 메소드 추가

--- src/main/java/cube8540/oauth/authentication/credentials/oauth/scope/domain/OAuth2Scope.java
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/scope/domain/OAuth2Scope.java
@@ -1,17 +1,26 @@
 package cube8540.oauth.authentication.credentials.oauth.scope.domain;
 
+import cube8540.oauth.authentication.credentials.authority.domain.AuthorityCode;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.AttributeOverride;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @ToString
@@ -28,8 +37,35 @@ public class OAuth2Scope extends AbstractAggregateRoot<OAuth2Scope> {
     @Column(name = "description", length = 64)
     private String description;
 
+    @ElementCollection
+    @CollectionTable(name = "oauth2_scope_accessible_authority", joinColumns = @JoinColumn(name = "scope_id", nullable = false))
+    @AttributeOverride(name = "value", column = @Column(name = "authority", length = 32, nullable = false))
+    private Set<AuthorityCode> accessibleAuthority;
+
     public OAuth2Scope(String scopeId, String description) {
         this.id = new OAuth2ScopeId(scopeId);
         this.description = description;
+    }
+
+    public void addAccessibleAuthority(AuthorityCode authority) {
+        if (this.accessibleAuthority == null) {
+            this.accessibleAuthority = new HashSet<>();
+        }
+        this.accessibleAuthority.add(authority);
+    }
+
+    public void removeAccessibleAuthority(AuthorityCode code) {
+        if (this.accessibleAuthority != null) {
+            this.accessibleAuthority.remove(code);
+        }
+    }
+
+    public boolean isAccessible(Authentication authentication) {
+        if (this.accessibleAuthority == null) {
+            return false;
+        }
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).map(AuthorityCode::new)
+                .anyMatch(authority -> accessibleAuthority.contains(authority));
     }
 }
--- src/test/java/cube8540/oauth/authentication/credentials/oauth/scope/domain/OAuth2ScopeTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/scope/domain/OAuth2ScopeTest.java
@@ -1,11 +1,23 @@
 package cube8540.oauth.authentication.credentials.oauth.scope.domain;
 
+import cube8540.oauth.authentication.credentials.authority.domain.AuthorityCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 @DisplayName("OAuth2 스코프 테스트")
 class OAuth2ScopeTest {
@@ -37,4 +49,162 @@ class OAuth2ScopeTest {
         }
     }
 
+    @Nested
+    @DisplayName("접근 가능한 권한 추가")
+    class AddAccessibleAuthority {
+        private OAuth2Scope scope;
+
+        @BeforeEach
+        void setup() {
+            this.scope = new OAuth2Scope(RAW_SCOPE_ID, DESCRIPTION);
+        }
+
+        @Nested
+        @DisplayName("새 접근 가능한 권한을 추가했을시")
+        class WhenAddedNewAccessibleAuthority {
+            private AuthorityCode code;
+
+            @BeforeEach
+            void setup() {
+                this.code = new AuthorityCode("AUTHORITY-CODE");
+            }
+
+            @Test
+            @DisplayName("인자로 받은 권한을 저장해야 한다.")
+            void shouldSaveGivenAuthority() {
+                scope.addAccessibleAuthority(code);
+
+                assertTrue(scope.getAccessibleAuthority().contains(code));
+            }
+        }
+
+        @Nested
+        @DisplayName("이미 저장되어 있는 권한을 추가할시")
+        class WhenAddedExistsAuthority {
+            private AuthorityCode code;
+
+            @BeforeEach
+            void setup() {
+                this.code = new AuthorityCode("AUTHORITY-CODE");
+                scope.addAccessibleAuthority(code);
+            }
+
+            @Test
+            @DisplayName("같은 권한은 하나만 저장되어 있어야 한다.")
+            void shouldStoredOnlyOneSameAuthority() {
+                scope.addAccessibleAuthority(code);
+
+                long count = scope.getAccessibleAuthority().stream().filter(code -> code.equals(this.code)).count();
+                assertEquals(1, count);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("접근 권한 제거")
+    class RemoveAccessibleAuthority {
+
+        private OAuth2Scope scope;
+
+        @BeforeEach
+        void setup() {
+            this.scope = new OAuth2Scope(RAW_SCOPE_ID, DESCRIPTION);
+        }
+
+        @Nested
+        @DisplayName("저장되지 않은 권한을 삭제할시")
+        class WhenRemovedNotStoredAuthority {
+            private AuthorityCode code;
+
+            @BeforeEach
+            void setup() {
+                this.code = new AuthorityCode("AUTHORITY-CODE");
+            }
+
+            @Test
+            @DisplayName("해당 요청은 무시한다.")
+            void shouldIgnoreRequesting() {
+                assertDoesNotThrow(() -> scope.removeAccessibleAuthority(code));
+            }
+        }
+
+        @Nested
+        @DisplayName("저장되어 있는 권한을 삭제시")
+        class WhenRemovedStoredAuthority {
+            private AuthorityCode code;
+
+            @BeforeEach
+            void setup() {
+                this.code = new AuthorityCode("AUTHORITY-CODE");
+                scope.addAccessibleAuthority(code);
+            }
+
+            @Test
+            @DisplayName("인자로 받은 권한을 삭제한다.")
+            void shouldRemovedRequestingAuthority() {
+                scope.removeAccessibleAuthority(code);
+
+                assertFalse(scope.getAccessibleAuthority().contains(code));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("접근 가능 확인")
+    class TestAccessible {
+        private OAuth2Scope scope;
+
+        @BeforeEach
+        void setup() {
+            this.scope = new OAuth2Scope(RAW_SCOPE_ID, DESCRIPTION);
+
+            this.scope.addAccessibleAuthority(new AuthorityCode("AUTHORITY-CODE-1"));
+            this.scope.addAccessibleAuthority(new AuthorityCode("AUTHORITY-CODE-2"));
+            this.scope.addAccessibleAuthority(new AuthorityCode("AUTHORITY-CODE-3"));
+        }
+
+        @Nested
+        @DisplayName("인증 정보에 접근 가능한 권한이 없을시")
+        class WhenAuthenticationNotHaveAccessibleAuthority {
+            private Authentication authentication;
+
+            @BeforeEach
+            void setup() {
+                this.authentication = mock(Authentication.class);
+
+                Collection<? extends GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("SCOPE-1"),
+                        new SimpleGrantedAuthority("SCOPE-2"),
+                        new SimpleGrantedAuthority("SCOPE-3"));
+                doReturn(authorities).when(authentication).getAuthorities();
+            }
+
+            @Test
+            @DisplayName("접근 가능 여부는 false가 반환되어야 한다.")
+            void shouldReturnsFalse() {
+                assertFalse(scope.isAccessible(authentication));
+            }
+        }
+
+        @Nested
+        @DisplayName("인증 정보에 접근 가능한 권한이 있을시")
+        class WhenAuthenticationHaveAccessibleAuthority {
+            private Authentication authentication;
+
+            @BeforeEach
+            void setup() {
+                this.authentication = mock(Authentication.class);
+
+                Collection<? extends GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("AUTHORITY-CODE-1"),
+                        new SimpleGrantedAuthority("SCOPE-2"),
+                        new SimpleGrantedAuthority("SCOPE-3"));
+                doReturn(authorities).when(authentication).getAuthorities();
+            }
+
+            @Test
+            @DisplayName("접근 가능 여부는 true가 반환되어야 한다.")
+            void shouldReturnsTrue() {
+                assertTrue(scope.isAccessible(authentication));
+            }
+        }
+    }
 }
