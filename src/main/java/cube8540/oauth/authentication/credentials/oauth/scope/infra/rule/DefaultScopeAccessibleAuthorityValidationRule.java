244e4c4c435afcefe43c27a5207ea13a2174ac2f
cube8540 <cube8540@gmail.com>
2020-02-18 22:53:13 +0900

    스코프 유효성 검사 클래스 추가

---
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/scope/infra/rule/DefaultScopeAccessibleAuthorityValidationRule.java
@@ -0,0 +1,49 @@
+package cube8540.oauth.authentication.credentials.oauth.scope.infra.rule;
+
+import cube8540.oauth.authentication.credentials.authority.AuthorityDetails;
+import cube8540.oauth.authentication.credentials.authority.AuthorityDetailsService;
+import cube8540.oauth.authentication.credentials.authority.domain.AuthorityCode;
+import cube8540.oauth.authentication.credentials.oauth.scope.domain.OAuth2Scope;
+import cube8540.validator.core.ValidationError;
+import cube8540.validator.core.ValidationRule;
+import lombok.Setter;
+
+import java.util.stream.Collectors;
+
+public class DefaultScopeAccessibleAuthorityValidationRule implements ValidationRule<OAuth2Scope> {
+
+    private static final String PROPERTY = "accessibleAuthority";
+    private static final String MESSAGE = "부여할 수 없는 접근 권한입니다.";
+
+    private String property;
+    private String message;
+
+    @Setter
+    private AuthorityDetailsService authorityService;
+
+    public DefaultScopeAccessibleAuthorityValidationRule() {
+        this(PROPERTY, MESSAGE);
+    }
+
+    public DefaultScopeAccessibleAuthorityValidationRule(String property, String message) {
+        this.property = property;
+        this.message = message;
+    }
+
+    @Override
+    public ValidationError error() {
+        return new ValidationError(property, message);
+    }
+
+    @Override
+    public boolean isValid(OAuth2Scope target) {
+        if (authorityService == null) {
+            return false;
+        }
+
+        return authorityService.getAuthorities().stream()
+                .map(AuthorityDetails::code).map(AuthorityCode::new)
+                .collect(Collectors.toList())
+                .containsAll(target.getAccessibleAuthority());
+    }
+}
