01426892cabac74b8e7a95203f58b1e44a506c05
cube8540 <cube8540@gmail.com>
2020-02-18 23:07:24 +0900

    스코프 관리 API 엔드 포인트 추가

---
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/scope/OAuth2ScopeConfigure.java
@@ -0,0 +1,32 @@
+package cube8540.oauth.authentication.credentials.oauth.scope;
+
+import cube8540.oauth.authentication.credentials.authority.AuthorityDetailsService;
+import cube8540.oauth.authentication.credentials.oauth.scope.application.DefaultScopeDetailsService;
+import cube8540.oauth.authentication.credentials.oauth.scope.domain.OAuth2ScopeRepository;
+import cube8540.oauth.authentication.credentials.oauth.scope.infra.DefaultOAuth2ScopeValidationPolicy;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OAuth2ScopeConfigure {
+
+    @Setter(onMethod_ = @Autowired)
+    private OAuth2ScopeRepository scopeRepository;
+
+    @Setter(onMethod_ = @Autowired)
+    private AuthorityDetailsService authorityService;
+
+    @Bean
+    public DefaultScopeDetailsService defaultScopeDetailsService() {
+        DefaultScopeDetailsService service = new DefaultScopeDetailsService(scopeRepository);
+
+        DefaultOAuth2ScopeValidationPolicy policy = new DefaultOAuth2ScopeValidationPolicy();
+        policy.setAuthorityService(authorityService);
+
+        service.setValidationPolicy(policy);
+        return service;
+    }
+
+}
