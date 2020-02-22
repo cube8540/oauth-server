d7e60ce5a3bcc7da8eb6b263e41412fa9794ea10
cube8540 <cube8540@gmail.com>
2020-02-22 16:16:06 +0900

    기본 에러 관련 클래스 추가 및 수정

---
+++ src/main/java/cube8540/oauth/authentication/error/ErrorCodes.java
@@ -0,0 +1,7 @@
+package cube8540.oauth.authentication.error;
+
+public interface ErrorCodes {
+
+    String SERVER_ERROR = "server_error";
+
+}
--- src/main/java/cube8540/oauth/authentication/error/ExceptionTranslator.java
+++ src/main/java/cube8540/oauth/authentication/error/ExceptionTranslator.java
@@ -1,10 +1,17 @@
 package cube8540.oauth.authentication.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.io.Serializable;
 
 public interface ExceptionTranslator<T extends Serializable> {
+
+    ErrorMessage<String> UNKNOWN_SERVER_ERROR = ErrorMessage.instance(ErrorCodes.SERVER_ERROR, "unknown server error");
+
+    default <B> ResponseEntity<B> response(HttpStatus status, B body) {
+        return new ResponseEntity<>(body, status);
+    }
 
     ResponseEntity<T> translate(Exception exception);
 
