6d4c708072643f89bb02e3e5e6e1d1e6e830f19d
cube8540 <cube8540@gmail.com>
2020-02-25 23:59:05 +0900

    유저 application 레이어 테스트 코드 리펙토링

--- src/test/java/cube8540/oauth/authentication/users/application/DefaultUserManagementServiceTest.java
+++ src/test/java/cube8540/oauth/authentication/users/application/DefaultUserManagementServiceTest.java
@@ -1,7 +1,6 @@
 package cube8540.oauth.authentication.users.application;
 
 import cube8540.oauth.authentication.users.domain.User;
-import cube8540.oauth.authentication.users.domain.UserEmail;
 import cube8540.oauth.authentication.users.domain.UserRepository;
 import cube8540.oauth.authentication.users.domain.UserValidationPolicy;
 import cube8540.oauth.authentication.users.error.UserErrorCodes;
@@ -16,53 +15,37 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.EMAIL;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.ENCODED_PASSWORD;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.PASSWORD;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.RAW_EMAIL;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.configDefaultMockUser;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.mockPasswordEncoder;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.mockUserRepository;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.mockValidationPolicy;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.mockValidationRule;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @DisplayName("기본 유저 계정 관리 서비스 테스트")
 class DefaultUserManagementServiceTest {
-
-    private static final String RAW_EMAIL = "email@email.com";
-    private static final UserEmail EMAIL = new UserEmail(RAW_EMAIL);
-
-    private static final String RAW_PASSWORD = "Password1234!@#$";
-
-    private static final String ENCODING_PASSWORD = "$2a$10$MrsAcjEPfD4ktbWEb13SBu.lE2OfGWZ2NPqgUoSTeWA7bvh9.k3WC";
-
-    private static final LocalDateTime REGISTERED_AT = LocalDateTime.of(2020, 2, 8, 15, 38);
-
-    private PasswordEncoder encoder;
-    private UserRepository userRepository;
-    private DefaultUserManagementService service;
-
-    @BeforeEach
-    void setup() {
-        this.userRepository = mock(UserRepository.class);
-        this.encoder = mock(PasswordEncoder.class);
-        this.service = new DefaultUserManagementService(userRepository, encoder);
-    }
 
     @Nested
     @DisplayName("유저 카운팅")
     class UserCounting {
         private long randomCount;
+        private DefaultUserManagementService service;
 
         @BeforeEach
         void setup() {
             this.randomCount = (long) (Math.random() * 100);
-            when(userRepository.countByEmail(EMAIL)).thenReturn(randomCount);
+
+            UserRepository repository = mockUserRepository().countUser(randomCount).build();
+
+            this.service = new DefaultUserManagementService(repository, mockPasswordEncoder().build());
         }
 
         @Test
@@ -80,15 +63,10 @@ class DefaultUserManagementServiceTest {
 
         @Nested
         @DisplayName("찾고 싶은 유저가 저장소에 없을시")
-        class WhenUserNotFound {
-
-            @BeforeEach
-            void setup() {
-                when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.empty());
-            }
+        class WhenNotRegisteredUserInRepository extends UserNotFoundSetup {
 
             @Test
-            @DisplayName("UserNotFoundException이 발생해야 한다.")
+            @DisplayName("UserNotFoundException 이 발생해야 한다.")
             void shouldThrowsUserNotFoundException() {
                 assertThrows(UserNotFoundException.class, () -> service.loadUserProfile(RAW_EMAIL));
             }
@@ -99,31 +77,24 @@ class DefaultUserManagementServiceTest {
     @DisplayName("유저 등록")
     class RegisterUser {
 
-        private UserRegisterRequest registerRequest;
-
-        @BeforeEach
-        void setup() {
-            this.registerRequest = new UserRegisterRequest(RAW_EMAIL, RAW_PASSWORD);
-        }
-
         @Nested
         @DisplayName("저장소에 이미 저장된 유저 이메일일시")
         class WhenExistingEmailInRepository {
+            private UserRegisterRequest registerRequest;
+
+            private DefaultUserManagementService service;
 
             @BeforeEach
             void setup() {
-                when(userRepository.countByEmail(new UserEmail(RAW_EMAIL))).thenReturn(1L);
+                UserRepository repository = mockUserRepository().countUser(1L).build();
+
+                this.registerRequest = new UserRegisterRequest(RAW_EMAIL, PASSWORD);
+                this.service = new DefaultUserManagementService(repository, mockPasswordEncoder().build());
             }
 
             @Test
-            @DisplayName("UserRegisterException이 발생해야 한다.")
-            void shouldThrowsUserRegisterException() {
-                assertThrows(UserRegisterException.class, () -> service.registerUser(registerRequest));
-            }
-
-            @Test
-            @DisplayName("에러 코드는 EXISTS_IDENTIFIER 이어야 한다.")
-            void shouldErrorCodeIsExistsIdentifier() {
+            @DisplayName("에러가 발생해야 하며, 에러 코드는 EXISTS_IDENTIFIER 이어야 한다.")
+            void shouldThrowsErrorAndErrorCodeIsExistsIdentifier() {
                 UserRegisterException e = assertThrows(UserRegisterException.class, () -> service.registerUser(registerRequest));
                 assertEquals(UserErrorCodes.EXISTS_IDENTIFIER, e.getCode());
             }
@@ -132,39 +103,37 @@ class DefaultUserManagementServiceTest {
         @Nested
         @DisplayName("저장소에 저장되지 않은 유저일시")
         class WhenNotRegisterInRepository {
-
+            private UserRepository repository;
             private ValidationRule<User> emailRule;
             private ValidationRule<User> passwordRule;
 
+            private UserRegisterRequest registerRequest;
+
+            private DefaultUserManagementService service;
+
             @BeforeEach
-            @SuppressWarnings("unchecked")
             void setup() {
-                UserValidationPolicy policy = mock(UserValidationPolicy.class);
-                this.emailRule = mock(ValidationRule.class);
-                this.passwordRule = mock(ValidationRule.class);
+                this.repository = mockUserRepository().countUser(0).build();
+                this.emailRule = mockValidationRule().configReturnsTrue().build();
+                this.passwordRule = mockValidationRule().configReturnsTrue().build();
 
-                when(policy.emailRule()).thenReturn(emailRule);
-                when(policy.passwordRule()).thenReturn(passwordRule);
-                when(userRepository.countByEmail(new UserEmail(RAW_EMAIL))).thenReturn(0L);
-                when(encoder.encode(RAW_PASSWORD)).thenReturn(ENCODING_PASSWORD);
-                when(emailRule.isValid(any(User.class))).thenReturn(true);
-                when(passwordRule.isValid(any(User.class))).thenReturn(true);
+                UserValidationPolicy policy = mockValidationPolicy()
+                        .email(emailRule).password(passwordRule).build();
+                PasswordEncoder encoder = mockPasswordEncoder().encode().build();
 
-                doAnswer(returnsFirstArg()).when(userRepository).save(isA(User.class));
+                this.service = new DefaultUserManagementService(repository, encoder);
+                this.service.setValidationPolicy(policy);
 
-                service.setValidationPolicy(policy);
+                this.registerRequest = new UserRegisterRequest(RAW_EMAIL, PASSWORD);
             }
 
             @Test
-            @DisplayName("요청 받은 유저 이메일을 유효성 검사 후 저장해야 한다.")
+            @DisplayName("요청 받은 유저 이메일을 유효성 검사를 해야 한다.")
             void shouldSaveRequestingUserEmailAfterValidation() {
                 ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
-                InOrder inOrder = inOrder(emailRule, userRepository);
 
                 service.registerUser(registerRequest);
-                inOrder.verify(emailRule, times(1)).isValid(userCaptor.capture());
-                inOrder.verify(userRepository, times(1)).save(userCaptor.capture());
-                assertEquals(userCaptor.getAllValues().get(0), userCaptor.getAllValues().get(1));
+                verifySaveAfterValidation(emailRule, userCaptor);
                 assertEquals(EMAIL, userCaptor.getValue().getEmail());
             }
 
@@ -172,13 +141,17 @@ class DefaultUserManagementServiceTest {
             @DisplayName("요청 받은 유저 패스워드 유효성 검사 후 암호화 하여 저장해야 한다.")
             void shouldSaveEncodedRequestingUserPasswordAfterValidation() {
                 ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
-                InOrder inOrder = inOrder(passwordRule, userRepository);
 
                 service.registerUser(registerRequest);
-                inOrder.verify(passwordRule, times(1)).isValid(userCaptor.capture());
-                inOrder.verify(userRepository, times(1)).save(userCaptor.capture());
-                assertEquals(userCaptor.getAllValues().get(0), userCaptor.getAllValues().get(1));
-                assertEquals(ENCODING_PASSWORD, userCaptor.getValue().getPassword());
+                verifySaveAfterValidation(passwordRule, userCaptor);
+                assertEquals(ENCODED_PASSWORD, userCaptor.getValue().getPassword());
+            }
+
+            private void verifySaveAfterValidation(ValidationRule<User> rule, ArgumentCaptor<User> argumentCaptor) {
+                InOrder inOrder = inOrder(rule, repository);
+                inOrder.verify(rule, times(1)).isValid(argumentCaptor.capture());
+                inOrder.verify(repository, times(1)).save(argumentCaptor.capture());
+                assertEquals(argumentCaptor.getAllValues().get(0), argumentCaptor.getAllValues().get(1));
             }
         }
     }
@@ -189,15 +162,10 @@ class DefaultUserManagementServiceTest {
 
         @Nested
         @DisplayName("저장소에 저장되있지 않은 유저일시")
-        class WhenNotRegisterUserInRepository {
-
-            @BeforeEach
-            void setup() {
-                when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.empty());
-            }
+        class WhenNotRegisterUserInRepository extends UserNotFoundSetup {
 
             @Test
-            @DisplayName("UserNotFoundExceptio이 발생해야 한다.")
+            @DisplayName("UserNotFoundException 이 발생해야 한다.")
             void shouldThrowsUserNotFoundException() {
                 assertThrows(UserNotFoundException.class, () -> service.removeUser(RAW_EMAIL));
             }
@@ -207,14 +175,16 @@ class DefaultUserManagementServiceTest {
         @DisplayName("저장소에 저장되어있는 유저일시")
         class WhenRegisterUserInRepository {
             private User user;
+            private UserRepository repository;
+
+            private UserManagementService service;
 
             @BeforeEach
             void setup() {
-                this.user = mock(User.class);
+                this.user = configDefaultMockUser().build();
+                this.repository = mockUserRepository().registerUser(user).build();
 
-                when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.of(user));
-                when(user.getEmail()).thenReturn(EMAIL);
-                when(user.getRegisteredAt()).thenReturn(REGISTERED_AT);
+                this.service = new DefaultUserManagementService(repository, mockPasswordEncoder().build());
             }
 
             @Test
@@ -223,9 +193,20 @@ class DefaultUserManagementServiceTest {
                 ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
 
                 service.removeUser(RAW_EMAIL);
-                verify(userRepository, times(1)).delete(userCaptor.capture());
+                verify(repository, times(1)).delete(userCaptor.capture());
                 assertEquals(user, userCaptor.getValue());
             }
+        }
+    }
+
+    abstract static class UserNotFoundSetup {
+        protected DefaultUserManagementService service;
+
+        @BeforeEach
+        void setup() {
+            UserRepository repository = mockUserRepository().emptyUser().build();
+
+            this.service = new DefaultUserManagementService(repository, mockPasswordEncoder().build());
         }
     }
 }
--- src/test/java/cube8540/oauth/authentication/users/application/DefaultUserServiceTest.java
+++ src/test/java/cube8540/oauth/authentication/users/application/DefaultUserServiceTest.java
@@ -1,140 +1,104 @@
 package cube8540.oauth.authentication.users.application;
 
-import cube8540.oauth.authentication.credentials.authority.domain.AuthorityCode;
 import cube8540.oauth.authentication.users.domain.User;
-import cube8540.oauth.authentication.users.domain.UserEmail;
 import cube8540.oauth.authentication.users.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.AUTHORITIES;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.RAW_EMAIL;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.configDefaultMockUser;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.convertGrantAuthority;
+import static cube8540.oauth.authentication.users.application.UserApplicationTestHelper.mockUserRepository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @DisplayName("기본 유저 서비스 테스트")
 class DefaultUserServiceTest {
 
-    private UserRepository repository;
-    private DefaultUserService service;
-
-    private User user;
-
-    @BeforeEach
-    void setup() {
-        this.user = mock(User.class);
-        this.repository = mock(UserRepository.class);
-        this.service = new DefaultUserService(repository);
-    }
-
     @Nested
     @DisplayName("저장소의 유저를 로딩")
     class WhenLoadingUser {
-        private UserEmail email = new UserEmail("email@email.com");
-        private String password = "password";
-        private Set<AuthorityCode> authorityCodes = new HashSet<>(Arrays.asList(
-                new AuthorityCode("CODE-1"), new AuthorityCode("CODE-2"), new AuthorityCode("CODE-3")));
-
-        @BeforeEach
-        void setup() {
-            when(user.getEmail()).thenReturn(email);
-            when(user.getPassword()).thenReturn(password);
-            when(user.getAuthorities()).thenReturn(authorityCodes);
-            when(repository.findByEmail(any())).thenReturn(Optional.empty());
-        }
 
         @Nested
         @DisplayName("저장소에 유저가 없을시")
-        class WhenNotFoundUser {
+        class WhenNotRegisteredUserInRepository {
+            private DefaultUserService service;
+
+            @BeforeEach
+            void setup() {
+                UserRepository repository = mockUserRepository().emptyUser().build();
+                this.service = new DefaultUserService(repository);
+            }
 
             @Test
-            @DisplayName("UsernameNotFoundException이 발생해야 한다.")
+            @DisplayName("UsernameNotFoundException 이 발생해야 한다.")
             void shouldThrowsUsernameNotFoundException() {
-                assertThrows(UsernameNotFoundException.class, () -> service.loadUserByUsername("email@email.com"));
+                assertThrows(UsernameNotFoundException.class, () -> service.loadUserByUsername(RAW_EMAIL));
             }
         }
 
         @Nested
         @DisplayName("저장소에 유저가 있을시")
-        class WhenFoundUser {
-
-            @BeforeEach
-            void setup() {
-                when(repository.findByEmail(email)).thenReturn(Optional.of(user));
-            }
-
-            @Test
-            @DisplayName("저장소에서 찾은 유저의 이메일을 반환해야 한다.")
-            void shouldReturnsUserEmail() {
-                UserDetails result = service.loadUserByUsername("email@email.com");
-
-                assertEquals("email@email.com", result.getUsername());
-            }
-
-            @Test
-            @DisplayName("저장소에서 찾은 유저의 패스워드를 반환해야 한다.")
-            void shouldReturnsUserPassword() {
-                UserDetails result = service.loadUserByUsername("email@email.com");
-
-                assertEquals("password", result.getPassword());
-            }
-
-            @Test
-            @DisplayName("저장소에서 찾은 유저의 권한을 반환해야 한다.")
-            void shouldReturnsGrantedAuthority() {
-                UserDetails result = service.loadUserByUsername("email@email.com");
-
-                Set<GrantedAuthority> expectedAuthorities = authorityCodes.stream()
-                        .map(auth -> new SimpleGrantedAuthority(auth.getValue()))
-                        .collect(Collectors.toSet());
-                assertEquals(expectedAuthorities, result.getAuthorities());
-            }
+        class WhenRegisteredUserInRepository {
 
             @Nested
-            @DisplayName("찾은 유저가 인증을 받지 않았을시")
-            class WhenFindUserIsNotCredentials {
+            @DisplayName("등록된 유저가 인증을 받지 않은 상태일시")
+            class WhenRegisteredUserIsNotCertified {
+                private DefaultUserService service;
 
                 @BeforeEach
                 void setup() {
-                    when(user.getAuthorities()).thenReturn(null);
+                    User user = configDefaultMockUser().build();
+                    UserRepository repository = mockUserRepository().registerUser(user).build();
+                    this.service = new DefaultUserService(repository);
                 }
 
                 @Test
                 @DisplayName("계정의 잠금된 설정으로 반환되어야 한다.")
                 void shouldAccountIsNotLocked() {
-                    UserDetails result = service.loadUserByUsername("email@email.com");
+                    UserDetails result = service.loadUserByUsername(RAW_EMAIL);
 
-                    boolean nonLocked = result.isAccountNonLocked();
-                    assertFalse(nonLocked);
+                    assertFalse(result.isAccountNonLocked());
                 }
             }
 
             @Nested
-            @DisplayName("찾은 유저가 인증을 받았을시")
-            class WhenFindUserIsCredentials {
+            @DisplayName("등록된 유저가 인증을 받은 상태일시")
+            class WhenRegisteredUserIsCertified {
+                private DefaultUserService service;
+
+                @BeforeEach
+                void setup() {
+                    User user = configDefaultMockUser().authorities().build();
+                    UserRepository repository = mockUserRepository().registerUser(user).build();
+                    this.service = new DefaultUserService(repository);
+                }
+
+                @Test
+                @DisplayName("저장소에서 찾은 유저의 권한을 반환해야 한다.")
+                void shouldReturnsGrantedAuthority() {
+                    UserDetails result = service.loadUserByUsername(RAW_EMAIL);
+
+                    Set<GrantedAuthority> expectedAuthorities = convertGrantAuthority(AUTHORITIES);
+                    assertEquals(expectedAuthorities, result.getAuthorities());
+                }
 
                 @Test
                 @DisplayName("계정의 잠금되지 않은 설정으로 반환되어야 한다.")
                 void shouldAccountIsNotLocked() {
-                    UserDetails result = service.loadUserByUsername("email@email.com");
+                    UserDetails result = service.loadUserByUsername(RAW_EMAIL);
 
-                    boolean nonLocked = result.isAccountNonLocked();
-                    assertTrue(nonLocked);
+                    assertTrue(result.isAccountNonLocked());
                 }
             }
         }
