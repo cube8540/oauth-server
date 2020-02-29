124fa775d206d60f1b76cf32b8bc6a86ee35b734
cube8540 <cube8540@gmail.com>
2020-02-29 22:27:00 +0900

    OAuth2 클라이언트 유효성 검사 테스트 클래스 리팩토링

--- src/test/java/cube8540/oauth/authentication/credentials/oauth/client/infra/rule/DefaultClientNameValidationRuleTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/client/infra/rule/DefaultClientNameValidationRuleTest.java
@@ -3,10 +3,9 @@ package cube8540.oauth.authentication.credentials.oauth.client.infra.rule;
 import cube8540.oauth.authentication.credentials.oauth.client.domain.OAuth2Client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -20,41 +19,13 @@ class DefaultClientNameValidationRuleTest {
         this.rule = new DefaultClientNameValidationRule();
     }
 
-    @Nested
-    @DisplayName("클라이언트명이 null일시")
-    class WhenClientNameIsNull {
+    @Test
+    @DisplayName("클라이언트명이 null 일시 유효성 검사 결과는 false 가 반환되어야 한다.")
+    void clientNameIsNullValidationShouldReturnsFalse() {
+        OAuth2Client client = mock(OAuth2Client.class);
 
-        private OAuth2Client client;
+        when(client.getClientName()).thenReturn(null);
 
-        @BeforeEach
-        void setup() {
-            this.client = mock(OAuth2Client.class);
-        }
-
-        @Test
-        @DisplayName("유효성 검사시 false가 반환되어야 한다.")
-        void shouldReturnsFalse() {
-            assertFalse(rule.isValid(client));
-        }
-    }
-
-    @Nested
-    @DisplayName("클라이언트명이 null이 아닐시")
-    class WhenClientNameIsNotNull {
-        private OAuth2Client client;
-
-        @BeforeEach
-        void setup() {
-            String clientName = "client name";
-
-            this.client = mock(OAuth2Client.class);
-            when(client.getClientName()).thenReturn(clientName);
-        }
-
-        @Test
-        @DisplayName("유효성 검사시 true가 반환되어야 한다.")
-        void shouldReturnsTrue() {
-            assertTrue(rule.isValid(client));
-        }
+        assertFalse(rule.isValid(client));
     }
 }
--- src/test/java/cube8540/oauth/authentication/credentials/oauth/client/infra/rule/DefaultOAuth2ClientOwnerValidationRuleTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/client/infra/rule/DefaultOAuth2ClientOwnerValidationRuleTest.java
@@ -1,13 +1,11 @@
 package cube8540.oauth.authentication.credentials.oauth.client.infra.rule;
 
 import cube8540.oauth.authentication.credentials.oauth.client.domain.OAuth2Client;
-import cube8540.oauth.authentication.users.domain.UserEmail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -21,42 +19,14 @@ class DefaultOAuth2ClientOwnerValidationRuleTest {
         this.rule = new DefaultOAuth2ClientOwnerValidationRule();
     }
 
-    @Nested
-    @DisplayName("클라이언트의 소유자가 null일시")
-    class WhenClientOwnerIsNull {
+    @Test
+    @DisplayName("클라이언트의 소유자가 null 일시 유효성 검사 결과는 false 가 반환되여야 한다.")
+    void clientOwnerNullValidationShouldReturnsFalse() {
+        OAuth2Client client = mock(OAuth2Client.class);
 
-        private OAuth2Client client;
+        when(client.getOwner()).thenReturn(null);
 
-        @BeforeEach
-        void setup() {
-            this.client = mock(OAuth2Client.class);
-        }
-
-        @Test
-        @DisplayName("유효성 검사시 false가 반환되어야 한다.")
-        void shouldReturnsFalse() {
-            assertFalse(rule.isValid(client));
-        }
-    }
-
-    @Nested
-    @DisplayName("클라이언트 소유자가 null이 아닐시")
-    class WhenClientOwnerIsNotNull {
-
-        private OAuth2Client client;
-
-        @BeforeEach
-        void setup() {
-            this.client = mock(OAuth2Client.class);
-
-            when(client.getOwner()).thenReturn(new UserEmail("email@email.com"));
-        }
-
-        @Test
-        @DisplayName("유효성 검사시 true가 반환되어야 한다.")
-        void shouldReturnsTrue() {
-            assertTrue(rule.isValid(client));
-        }
+        assertFalse(rule.isValid(client));
     }
 
 }
