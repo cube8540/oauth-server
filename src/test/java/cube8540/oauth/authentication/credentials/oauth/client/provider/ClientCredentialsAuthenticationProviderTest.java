664e27a7e063f637228087cd82d9b18a67487b70
cube8540 <cube8540@gmail.com>
2020-03-01 16:51:26 +0900

    ClientCredentialsAuthenticationProvider 반환되는 인증 정보 변경 및 테스트 코드 리펙토링

--- src/test/java/cube8540/oauth/authentication/credentials/oauth/client/provider/ClientCredentialsAuthenticationProviderTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/client/provider/ClientCredentialsAuthenticationProviderTest.java
@@ -2,7 +2,6 @@ package cube8540.oauth.authentication.credentials.oauth.client.provider;
 
 import cube8540.oauth.authentication.credentials.oauth.client.OAuth2ClientDetails;
 import cube8540.oauth.authentication.credentials.oauth.client.OAuth2ClientDetailsService;
-import cube8540.oauth.authentication.credentials.oauth.client.error.ClientNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,119 +13,85 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static cube8540.oauth.authentication.credentials.oauth.client.provider.ClientCredentialsProviderTestHelper.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("클라이언트 인증 제공 클래스 테스트")
 class ClientCredentialsAuthenticationProviderTest {
-
-    private static final String CLIENT_ID = "CLIENT_ID";
-    private static final String CLIENT_SECRET = "CLIENT_SECRET";
-    private static final String ENCODING_CLIENT_SECRET = "ENCODING_CLIENT_SECRET";
-
-    private OAuth2ClientDetailsService service;
-    private PasswordEncoder encoder;
-
-    private ClientCredentialsAuthenticationProvider provider;
-
-    @BeforeEach
-    void setup() {
-        this.service = mock(OAuth2ClientDetailsService.class);
-        this.encoder = mock(PasswordEncoder.class);
-        this.provider = new ClientCredentialsAuthenticationProvider(service, encoder);
-    }
-
-    @Nested
-    @DisplayName("인증을 지원하는 토큰 타입 테스트")
-    class SupportedTokenType {
-
-        @Nested
-        @DisplayName("매개변수로 받은 타입이 ClientCredentialsToken이 아닐시")
-        class WhenArgumentTypeIsNotClientCredentialsToken {
-
-            @Test
-            @DisplayName("지원 여부는 false가 반환되어야 한다.")
-            void shouldSupportedReturnsFalse() {
-                assertFalse(provider.supports(Object.class));
-                assertFalse(provider.supports(Authentication.class));
-            }
-        }
-
-        @Nested
-        @DisplayName("매개 변수로 받은 타입이 ClientCredentialsToken일시")
-        class WhenArgumentTypeIsClientCredentialsToken {
-
-            @Test
-            @DisplayName("지원 여부는 true가 반환되어야 한다.")
-            void shouldSupportedReturnsTrue() {
-                assertTrue(provider.supports(ClientCredentialsToken.class));
-            }
-        }
-    }
 
     @Nested
     @DisplayName("인증 진행")
     class AuthenticationProcessing {
 
-        private ClientCredentialsToken token;
-
-        @BeforeEach
-        void setup() {
-            this.token = new ClientCredentialsToken(CLIENT_ID, CLIENT_SECRET);
-        }
-
         @Nested
         @DisplayName("인증을 요청한 클라이언트를 찾을 수 없을시")
         class WhenClientNotFound {
+            private ClientCredentialsToken token;
+
+            private ClientCredentialsAuthenticationProvider provider;
 
             @BeforeEach
             void setup() {
-                when(service.loadClientDetailsByClientId(any())).thenThrow(new ClientNotFoundException("client not found"));
+                this.token = new ClientCredentialsToken(RAW_CLIENT_ID, CLIENT_SECRET);
+
+                OAuth2ClientDetailsService service = mockOAuth2ClientDetailsService().emptyClient().build();
+                PasswordEncoder encoder = mockPasswordEncoder().build();
+
+                this.provider = new ClientCredentialsAuthenticationProvider(service, encoder);
             }
 
             @Test
-            @DisplayName("BadCredentialsExeption이 발생해야 한다.")
+            @DisplayName("BadCredentialsException 이 발생해야 한다.")
             void shouldThrowsBadCredentialsException() {
                 assertThrows(BadCredentialsException.class, () -> provider.authenticate(token));
             }
         }
 
         @Nested
-        @DisplayName("클라이언트 아이디가 null일시")
+        @DisplayName("클라이언트 아이디가 null 일시")
         class WhenPrincipalIsNull {
-
             private ClientCredentialsToken token;
+
+            private ClientCredentialsAuthenticationProvider provider;
 
             @BeforeEach
             void setup() {
                 this.token = new ClientCredentialsToken(null, CLIENT_SECRET);
+
+                OAuth2ClientDetailsService service = mockOAuth2ClientDetailsService()
+                        .registerClient(mockOAuth2ClientDetails().build()).build();
+                PasswordEncoder encoder = mockPasswordEncoder().build();
+
+                this.provider = new ClientCredentialsAuthenticationProvider(service, encoder);
             }
 
             @Test
-            @DisplayName("BadCredentialsExeption이 발생해야 한다.")
+            @DisplayName("BadCredentialsException 이 발생해야 한다.")
             void shouldThrowsBadCredentialsException() {
                 assertThrows(BadCredentialsException.class, () -> provider.authenticate(token));
             }
         }
 
         @Nested
-        @DisplayName("클라이언트 패스워드가 null일시")
+        @DisplayName("클라이언트 패스워드가 null 일시")
         class WhenCredentialsIsNull {
-
             private ClientCredentialsToken token;
+
+            private ClientCredentialsAuthenticationProvider provider;
 
             @BeforeEach
             void setup() {
-                this.token = new ClientCredentialsToken(CLIENT_ID, null);
+                this.token = new ClientCredentialsToken(RAW_CLIENT_ID, null);
+
+                OAuth2ClientDetailsService service = mockOAuth2ClientDetailsService()
+                        .registerClient(mockOAuth2ClientDetails().build()).build();
+                PasswordEncoder encoder = mockPasswordEncoder().build();
+
+                this.provider = new ClientCredentialsAuthenticationProvider(service, encoder);
             }
 
             @Test
-            @DisplayName("BadCredentialsExeption이 발생해야 한다.")
+            @DisplayName("BadCredentialsExeption 이 발생해야 한다.")
             void shouldThrowsBadCredentialsException() {
                 assertThrows(BadCredentialsException.class, () -> provider.authenticate(token));
             }
@@ -135,26 +100,27 @@ class ClientCredentialsAuthenticationProviderTest {
         @Nested
         @DisplayName("인증을 요청한 클라이언트의 정보를 찾을 수 있을시")
         class WhenClientFound {
-            private OAuth2ClientDetails details;
-
-            @BeforeEach
-            void setup() {
-                this.details = mock(OAuth2ClientDetails.class);
-                when(details.getClientId()).thenReturn(CLIENT_ID);
-                when(details.getClientSecret()).thenReturn(ENCODING_CLIENT_SECRET);
-                when(service.loadClientDetailsByClientId(CLIENT_ID)).thenReturn(details);
-            }
 
             @Nested
             @DisplayName("찾은 클라이언트의 패스워드와 입력받은 패스워드가 일치하지 않을시")
             class WhenPasswordNotMatched {
+                private ClientCredentialsToken token;
+
+                private ClientCredentialsAuthenticationProvider provider;
+
                 @BeforeEach
                 void setup() {
-                    when(encoder.matches(CLIENT_SECRET, ENCODING_CLIENT_SECRET)).thenReturn(false);
+                    this.token = new ClientCredentialsToken(RAW_CLIENT_ID, CLIENT_SECRET);
+
+                    OAuth2ClientDetails clientDetails = mockOAuth2ClientDetails().configDefault().build();
+                    OAuth2ClientDetailsService service = mockOAuth2ClientDetailsService().registerClient(clientDetails).build();
+                    PasswordEncoder encoder = mockPasswordEncoder().mismatches().build();
+
+                    this.provider = new ClientCredentialsAuthenticationProvider(service, encoder);
                 }
 
                 @Test
-                @DisplayName("BadCredentialsException이 발생해야 한다.")
+                @DisplayName("BadCredentialsException 이 발생해야 한다.")
                 void shouldThrowsBadCredentialsException() {
                     assertThrows(BadCredentialsException.class, () -> provider.authenticate(token));
                 }
@@ -163,9 +129,20 @@ class ClientCredentialsAuthenticationProviderTest {
             @Nested
             @DisplayName("찾은 클라이언트의 패스워드와 입력받은 패스워드가 일치할시")
             class WhenPasswordMatched {
+                private ClientCredentialsToken token;
+                private OAuth2ClientDetails clientDetails;
+
+                private ClientCredentialsAuthenticationProvider provider;
+
                 @BeforeEach
                 void setup() {
-                    when(encoder.matches(CLIENT_SECRET, ENCODING_CLIENT_SECRET)).thenReturn(true);
+                    this.token = new ClientCredentialsToken(RAW_CLIENT_ID, CLIENT_SECRET);
+                    this.clientDetails = mockOAuth2ClientDetails().configDefault().build();
+
+                    OAuth2ClientDetailsService service = mockOAuth2ClientDetailsService().registerClient(clientDetails).build();
+                    PasswordEncoder passwordEncoder = mockPasswordEncoder().encode().matches().build();
+
+                    this.provider = new ClientCredentialsAuthenticationProvider(service, passwordEncoder);
                 }
 
                 @Test
@@ -173,15 +150,15 @@ class ClientCredentialsAuthenticationProviderTest {
                 void shouldSaveAuthenticationClient() {
                     Authentication authentication = provider.authenticate(token);
 
-                    assertEquals(details, authentication.getPrincipal());
+                    assertEquals(clientDetails, authentication.getPrincipal());
                 }
 
                 @Test
-                @DisplayName("인증 받은 클라이언트의 원래 패스워드를 저장하여 반환해야 한다.")
-                void shouldSaveOriginalClientSecret() {
+                @DisplayName("인증 받은 클라이언트의 패스워드를 저장하여 반환해야 한다.")
+                void shouldSaveClientSecret() {
                     Authentication authentication = provider.authenticate(token);
 
-                    assertEquals(CLIENT_SECRET, authentication.getCredentials());
+                    assertEquals(ENCODING_CLIENT_SECRET, authentication.getCredentials());
                 }
 
                 @Test
@@ -193,7 +170,7 @@ class ClientCredentialsAuthenticationProviderTest {
                 }
 
                 @Test
-                @DisplayName("인증 여부는 true가 반환되어야 한다.")
+                @DisplayName("인증 여부는 true 가 반환되어야 한다.")
                 void shouldAuthenticatedReturnsTrue() {
                     Authentication authentication = provider.authenticate(token);
 
@@ -205,14 +182,20 @@ class ClientCredentialsAuthenticationProviderTest {
         @Nested
         @DisplayName("인증 중 예상하지 못한 예외가 발생했을시")
         class WhenThrowsRuntimeException {
+            private ClientCredentialsToken token;
+
+            private ClientCredentialsAuthenticationProvider provider;
 
             @BeforeEach
             void setup() {
-                when(service.loadClientDetailsByClientId(any())).thenThrow(new RuntimeException());
+                this.token = new ClientCredentialsToken(RAW_CLIENT_ID, CLIENT_SECRET);
+
+                OAuth2ClientDetailsService service = mockOAuth2ClientDetailsService().configThrows().build();
+                this.provider = new ClientCredentialsAuthenticationProvider(service, mockPasswordEncoder().build());
             }
 
             @Test
-            @DisplayName("발생한 예외를 InternalAuthenticationServiceException으로 감싸 반환해야 한다.")
+            @DisplayName("발생한 예외를 InternalAuthenticationServiceException 으로 감싸 반환해야 한다.")
             void shouldThrowsExceptionWithInternalAuthenticationServiceException() {
                 assertThrows(InternalAuthenticationServiceException.class, () -> provider.authenticate(token));
             }
