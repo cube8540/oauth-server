496bf11f2b429e06d749e45a8449d3530699148d
cube8540 <cube8540@gmail.com>
2020-03-10 01:14:19 +0900

    필요 없는 어노테이션 삭제

--- src/main/java/cube8540/oauth/authentication/credentials/oauth/client/application/OAuth2ClientModifyRequest.java
+++ src/main/java/cube8540/oauth/authentication/credentials/oauth/client/application/OAuth2ClientModifyRequest.java
@@ -1,15 +1,12 @@
 package cube8540.oauth.authentication.credentials.oauth.client.application;
 
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import lombok.Singular;
 import lombok.Value;
 
 import java.beans.ConstructorProperties;
 import java.util.List;
 
 @Value
-@Builder
 @RequiredArgsConstructor(onConstructor_ = @ConstructorProperties({
         "clientName", "newRedirectUris", "removeRedirectUri", "newGrantTypes", "removeGrantTypes", "newScopes", "removeScopes"
 }))
@@ -17,22 +14,16 @@ public class OAuth2ClientModifyRequest {
 
     private String clientName;
 
-    @Singular("newRedirectUri")
     private List<String> newRedirectUris;
 
-    @Singular("removeRedirectUri")
     private List<String> removeRedirectUris;
 
-    @Singular("newGrantType")
     private List<String> newGrantTypes;
 
-    @Singular("removeGrantType")
     private List<String> removeGrantTypes;
 
-    @Singular("newScope")
     private List<String> newScopes;
 
-    @Singular("removeScope")
     private List<String> removeScopes;
 
 }
