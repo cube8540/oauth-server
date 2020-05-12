1753415b4aebbf8da30f37ff05e7362bac868c5d
cube8540 <cube8540@gmail.com>
2020-05-13 01:44:54 +0900

    로그인시 삭제된 권한 도메인도 가져오는 버그 수정

--- src/main/java/cube8540/oauth/authentication/users/domain/UserRepository.java
+++ src/main/java/cube8540/oauth/authentication/users/domain/UserRepository.java
@@ -1,13 +1,11 @@
 package cube8540.oauth.authentication.users.domain;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, UserEmail> {
 
-    @EntityGraph(attributePaths = "authorities", type = EntityGraph.EntityGraphType.LOAD)
     Optional<User> findByEmail(UserEmail userEmail);
 
     Long countByEmail(UserEmail userEmail);
