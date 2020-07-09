3dc0c29f88ac461a089ba80b44fd2894dc799b32
cube8540 <cube8540@gmail.com>
2020-07-09 18:33:48 +0900

    스코프 관련 테스트 코드 수정

--- src/test/java/cube8540/oauth/authentication/credentials/oauth/scope/infra/rule/DefaultScopeAccessibleAuthorityValidationRuleTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/scope/infra/rule/DefaultScopeAccessibleAuthorityValidationRuleTest.java
@@ -1,12 +1,11 @@
 package cube8540.oauth.authentication.credentials.oauth.scope.infra.rule;
 
+import cube8540.oauth.authentication.credentials.AuthorityCode;
 import cube8540.oauth.authentication.credentials.AuthorityDetails;
 import cube8540.oauth.authentication.credentials.AuthorityDetailsService;
-import cube8540.oauth.authentication.credentials.AuthorityCode;
 import cube8540.oauth.authentication.credentials.oauth.scope.domain.OAuth2Scope;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -36,81 +35,44 @@ class DefaultScopeAccessibleAuthorityValidationRuleTest {
         this.rule.setScopeDetailsServices(detailsService);
     }
 
-    @Nested
-    @DisplayName("권한 서비스가 null 일시")
-    class WhenAuthorityDetailsServiceIsNull {
-        private OAuth2Scope scope;
+    @Test
+    @DisplayName("권한 서비스가 null 일시 유효성 검사")
+    void validationWithOutAuthorityService() {
+        OAuth2Scope scope = mock(OAuth2Scope.class);
 
-        @BeforeEach
-        void setup() {
-            this.scope = mock(OAuth2Scope.class);
-            rule.setScopeDetailsServices(null);
-        }
+        rule.setScopeDetailsServices(null);
 
-        @Test
-        @DisplayName("유효성 검사 결과는 false가 반환되어야 한다.")
-        void shouldReturnsFalse() {
-            assertFalse(rule.isValid(scope));
-        }
+        assertFalse(rule.isValid(scope));
     }
 
-    @Nested
-    @DisplayName("스코프의 접근 권한중 존재하지 않는 접근 권한이 있을시")
-    class WhenScopesAuthorityContainsNotExistAuthority {
+    @Test
+    @DisplayName("스코프의 접근 권한중 존재 하지 않는 접근 권한이 있을시")
+    void scopeAuthorityHasNotExistsAuthority() {
+        OAuth2Scope scope = mock(OAuth2Scope.class);
+        List<AuthorityDetails> details = Arrays.asList(mocking("AUTH-1"), mocking("AUTH-2"));
 
-        private OAuth2Scope scope;
+        when(scope.getAccessibleAuthority()).thenReturn(GIVEN_SCOPES);
+        when(detailsService.loadAuthorityByAuthorityCodes(RAW_GIVEN_SCOPES)).thenReturn(details);
 
-        @BeforeEach
-        void setup() {
-            this.scope = mock(OAuth2Scope.class);
-
-            List<AuthorityDetails> details = Arrays.asList(mocking("AUTH-1"), mocking("AUTH-2"));
-
-            when(scope.getAccessibleAuthority()).thenReturn(GIVEN_SCOPES);
-            when(detailsService.loadAuthorityByAuthorityCodes(RAW_GIVEN_SCOPES)).thenReturn(details);
-        }
-
-        @Test
-        @DisplayName("유효성 검사 결과는 false가 반환되어야 한다.")
-        void shouldReturnsFalse() {
-            assertFalse(rule.isValid(scope));
-        }
-
-        private AuthorityDetails mocking(String code) {
-            AuthorityDetails details = mock(AuthorityDetails.class);
-
-            when(details.getCode()).thenReturn(code);
-            return details;
-        }
+        assertFalse(rule.isValid(scope));
     }
 
-    @Nested
-    @DisplayName("스코프의 접근 권한이 모두 존재하는 접근 권한일시")
-    class WhenScopesAuthorityAllExisting {
+    @Test
+    @DisplayName("스코프의 접근 권한이 모두 존재 하는 접근 권한 일시")
+    void scopeAuthorityIsAllExists() {
+        OAuth2Scope scope = mock(OAuth2Scope.class);
+        List<AuthorityDetails> details = Arrays.asList(mocking("AUTH-1"), mocking("AUTH-2"), mocking("AUTH-3"));
 
-        private OAuth2Scope scope;
+        when(scope.getAccessibleAuthority()).thenReturn(GIVEN_SCOPES);
+        when(detailsService.loadAuthorityByAuthorityCodes(RAW_GIVEN_SCOPES)).thenReturn(details);
 
-        @BeforeEach
-        void setup() {
-            this.scope = mock(OAuth2Scope.class);
+        assertTrue(rule.isValid(scope));
+    }
 
-            List<AuthorityDetails> details = Arrays.asList(mocking("AUTH-1"), mocking("AUTH-2"), mocking("AUTH-3"));
+    private AuthorityDetails mocking(String code) {
+        AuthorityDetails details = mock(AuthorityDetails.class);
 
-            when(scope.getAccessibleAuthority()).thenReturn(GIVEN_SCOPES);
-            when(detailsService.loadAuthorityByAuthorityCodes(RAW_GIVEN_SCOPES)).thenReturn(details);
-        }
-
-        @Test
-        @DisplayName("유효성 검사 결과는 true가 반환되어야 한다.")
-        void shouldReturnsTrue() {
-            assertTrue(rule.isValid(scope));
-        }
-
-        private AuthorityDetails mocking(String code) {
-            AuthorityDetails details = mock(AuthorityDetails.class);
-
-            when(details.getCode()).thenReturn(code);
-            return details;
-        }
+        when(details.getCode()).thenReturn(code);
+        return details;
     }
 }
