dd44e575d95844191e8984fd316064e2db8da0c8
cube8540 <cube8540@gmail.com>
2020-07-10 11:12:38 +0900

    Oauth2 에러 관련 테스트 코드 수정

--- src/test/java/cube8540/oauth/authentication/credentials/oauth/error/AbstractOAuth2AuthenticationExceptionTest.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/error/AbstractOAuth2AuthenticationExceptionTest.java
@@ -1,8 +1,6 @@
 package cube8540.oauth.authentication.credentials.oauth.error;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
@@ -15,50 +13,23 @@ class AbstractOAuth2AuthenticationExceptionTest {
     private static final String ERROR = OAuth2ErrorCodes.INVALID_CLIENT;
     private static final String ERROR_MESSAGE = "bad client credentials";
 
-    @Nested
-    @DisplayName("toString 테스트")
-    class ToString {
+    @Test
+    @DisplayName("에러 메시지가 없을떄, toString 메소드는 error_description 속성은 제외 된다")
+    void whenErrorMessageIsNullToStringMethodExcludeErrorDescription() {
+        OAuth2Error error = new OAuth2Error(ERROR);
+        AbstractOAuth2AuthenticationException exception = new AbstractOAuth2AuthenticationException(401, error) {};
 
-        @Nested
-        @DisplayName("에러 메시지가 없을시")
-        class WhenErrorMessageNull {
-            private OAuth2Error error;
-            private AbstractOAuth2AuthenticationException exception;
+        String result = exception.toString();
+        assertEquals("error=\"invalid_client\"", result);
+    }
 
-            @BeforeEach
-            void setup() {
-                this.error = new OAuth2Error(ERROR);
-                this.exception = new AbstractOAuth2AuthenticationException(401, error) {};
-            }
+    @Test
+    @DisplayName("toString 메소드는 erro와 error_description 속성이 모두 포함 되어야 한다")
+    void toStringMethodIncludeAllProperty() {
+        OAuth2Error error = new OAuth2Error(ERROR, ERROR_MESSAGE, null);
+        AbstractOAuth2AuthenticationException exception = new AbstractOAuth2AuthenticationException(401, error) {};
 
-            @Test
-            @DisplayName("toString시 error_description 속성은 제외되어야 한다.")
-            void shouldToStringExcludeErrorDescriptionProperty() {
-                String result = exception.toString();
-
-                assertEquals("error=\"invalid_client\"", result);
-            }
-        }
-
-        @Nested
-        @DisplayName("에러 메시지가 있을시")
-        class WhenErrorMessageNotNull {
-            private OAuth2Error error;
-            private AbstractOAuth2AuthenticationException exception;
-
-            @BeforeEach
-            void setup() {
-                this.error = new OAuth2Error(ERROR, ERROR_MESSAGE, null);
-                this.exception = new AbstractOAuth2AuthenticationException(401, error) {};
-            }
-
-            @Test
-            @DisplayName("toString시 error와 error_description 속성이 모두 포함되어야 한다.")
-            void shouldToStringIncludeAllProperty() {
-                String result = exception.toString();
-
-                assertEquals("error=\"invalid_client\", error_description=\"bad client credentials\"", result);
-            }
-        }
+        String result = exception.toString();
+        assertEquals("error=\"invalid_client\", error_description=\"bad client credentials\"", result);
     }
 }
