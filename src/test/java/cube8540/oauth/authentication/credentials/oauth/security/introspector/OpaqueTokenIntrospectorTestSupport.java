8f8b65d5beeec5c483a4e7f58ca8eb62161ffbfe
cube8540 <cube8540@gmail.com>
2020-08-18 20:31:36 +0900

    토큰 유효성 검사시 토큰을 발행한 클라이언트도 검사 하도록 수정

--- src/test/java/cube8540/oauth/authentication/credentials/oauth/security/introspector/OpaqueTokenIntrospectorTestSupport.java
+++ src/test/java/cube8540/oauth/authentication/credentials/oauth/security/introspector/OpaqueTokenIntrospectorTestSupport.java
@@ -4,9 +4,12 @@ import cube8540.oauth.authentication.credentials.AuthorityCode;
 import cube8540.oauth.authentication.credentials.oauth.client.domain.OAuth2ClientId;
 import cube8540.oauth.authentication.credentials.oauth.security.OAuth2AccessTokenDetails;
 import cube8540.oauth.authentication.credentials.oauth.security.OAuth2AccessTokenDetailsService;
+import cube8540.oauth.authentication.credentials.oauth.security.provider.ClientCredentialsToken;
 import cube8540.oauth.authentication.credentials.oauth.token.domain.OAuth2AccessTokenNotFoundException;
 import cube8540.oauth.authentication.credentials.oauth.token.domain.OAuth2TokenId;
 import cube8540.oauth.authentication.credentials.oauth.token.domain.PrincipalUsername;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -37,6 +40,8 @@ public class OpaqueTokenIntrospectorTestSupport {
 
     static final String RAW_CLIENT_ID = "CLIENT-ID";
     static final OAuth2ClientId CLIENT_ID = new OAuth2ClientId(RAW_CLIENT_ID);
+    static final String RAW_DIFFERENT_CLIENT_ID = "DIFFERENT_CLIENT_ID";
+    static final String CLIENT_SECRET = "CLIENT-SECRET";
 
     static final Set<String> RAW_SCOPES = new HashSet<>(Arrays.asList("SCOPE-1", "SCOPE-2", "SCOPE-3"));
     static final Set<AuthorityCode> SCOPES = RAW_SCOPES.stream().map(AuthorityCode::new).collect(Collectors.toSet());
@@ -56,6 +61,20 @@ public class OpaqueTokenIntrospectorTestSupport {
 
         when(token.getTokenValue()).thenReturn(RAW_ACCESS_TOKEN_ID);
         when(token.getClientId()).thenReturn(RAW_CLIENT_ID);
+        when(token.getUsername()).thenReturn(RAW_USERNAME);
+        when(token.getScopes()).thenReturn(RAW_SCOPES);
+        when(token.getExpiration()).thenReturn(EXPIRATION_DATETIME);
+        when(token.getAdditionalInformation()).thenReturn(ADDITIONAL_INFO);
+        when(token.isExpired()).thenReturn(false);
+
+        return token;
+    }
+
+    static OAuth2AccessTokenDetails makeDifferentClientAccessToken() {
+        OAuth2AccessTokenDetails token = mock(OAuth2AccessTokenDetails.class);
+
+        when(token.getTokenValue()).thenReturn(RAW_ACCESS_TOKEN_ID);
+        when(token.getClientId()).thenReturn(RAW_DIFFERENT_CLIENT_ID);
         when(token.getUsername()).thenReturn(RAW_USERNAME);
         when(token.getScopes()).thenReturn(RAW_SCOPES);
         when(token.getExpiration()).thenReturn(EXPIRATION_DATETIME);
@@ -85,5 +104,29 @@ public class OpaqueTokenIntrospectorTestSupport {
         when(service.readAccessTokenUser(RAW_ACCESS_TOKEN_ID)).thenReturn(user);
 
         return service;
+    }
+
+    static Authentication makeRequestAuthentication(String clientId, String clientSecret) {
+        return new ClientCredentialsToken(clientId, clientSecret);
+    }
+
+    static AuthenticationProvider makeAuthenticationProvider(Authentication authentication) {
+        AuthenticationProvider provider = mock(AuthenticationProvider.class);
+        Authentication auth = mock(Authentication.class);
+
+        when(provider.authenticate(authentication)).thenReturn(auth);
+        when(auth.getName()).thenReturn(authentication.getName());
+
+        return provider;
+    }
+
+    static AuthenticationProvider makeExceptionAuthenticationProvider(Authentication authentication) {
+        AuthenticationProvider provider = mock(AuthenticationProvider.class);
+        Authentication auth = mock(Authentication.class);
+
+        when(provider.authenticate(authentication)).thenThrow(new RuntimeException());
+        when(auth.getName()).thenReturn(authentication.getName());
+
+        return provider;
     }
 }
